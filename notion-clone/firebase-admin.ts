8e60bf2ed00b1bc1619bff2f749d2d3e5bfe03af
pantorn <longpantorn@gmail.com>
2026-03-15 22:14:32 +0700

    fix build error

--- notion-clone/firebase-admin.ts
+++ notion-clone/firebase-admin.ts
@@ -1,26 +1,15 @@
 import { initializeApp, App, getApps, getApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-// import serviceKey from "./service_key.json"; // Import JSON file
 
-const serviceKey = require("./service_key.json");
-
-// const serviceAccount = JSON.parse(
-//   Buffer.from(process.env.FIREBASE_SERVICE_KEY || "", "base64").toString()
-// );
+const serviceAccount = JSON.parse(
+	Buffer.from(process.env.FIREBASE_SERVICE_KEY || "", "base64").toString()
+);
 
 let app: App;
 
-// if (getApps().length === 0) {
-// 	app.initializeApp({
-// 		credential: cert(serviceAccount),
-// 	});
-// } else {
-// 	app = getApp();
-// }
-
 if (getApps().length === 0) {
 	app = initializeApp({
-		credential: cert(serviceKey),
+		credential: cert(serviceAccount),
 	});
 } else {
 	app = getApp();
