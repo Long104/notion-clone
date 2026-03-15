6adee483443b503c1e5feaf176683ac16a970352
pantorn <longpantorn@gmail.com>
2026-03-15 22:08:01 +0700

    fix build fail on portfolio

--- notion-clone/firebase-admin.ts
+++ notion-clone/firebase-admin.ts
@@ -4,7 +4,19 @@ import { getFirestore } from "firebase-admin/firestore";
 
 const serviceKey = require("./service_key.json");
 
+// const serviceAccount = JSON.parse(
+//   Buffer.from(process.env.FIREBASE_SERVICE_KEY || "", "base64").toString()
+// );
+
 let app: App;
+
+// if (getApps().length === 0) {
+// 	app.initializeApp({
+// 		credential: cert(serviceAccount),
+// 	});
+// } else {
+// 	app = getApp();
+// }
 
 if (getApps().length === 0) {
 	app = initializeApp({
@@ -17,3 +29,5 @@ if (getApps().length === 0) {
 const adminDb = getFirestore(app);
 
 export { app as adminApp, adminDb };
+
+
